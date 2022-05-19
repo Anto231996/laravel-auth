994bb18d20a770db951d4f41981a4fff31ebd814
Anto231996 <tony2396@hotmail.it>
2022-05-19 17:56:42 +0200

    add style

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,14 +4,14 @@
 <section class="container pt-5">
     <div class="row">
         @foreach ($posts as $post)
-        <div class="items col col-md-6 col-lg-4 mb-4">
+        <div class="items d-flex align-items-center flex-column col-4 mb-4">
             <a href="{{Route('admin.posts.show', $post)}}">
-                <img src="{{ $post->image_url }}" alt="">
+                <img src="{{ $post->image_url }}" alt="{{ $post->author }}</">
             </a>
             <div class="item-description text-center">
-                <h3>{{ $post->title }}</h3>
+                <h2>{{ $post->title }}</h2>
                 <h4>{{ $post->author }}</h4>
-                <h5>{{ $post->created_at }}</h5>
+                <h6>{{ $post->created_at }}</h6>
             </div>
         </div>
         @endforeach
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <section class="container pt-5">
     <div class="row">
-        <div class="items col col-md-6 col-lg-4 mb-4">
-            <img src="{{ $post->image_url }}" alt="">
+        <div class="items d-flex align-items-center flex-column col">
+            <img src="{{ $post->image_url }}" alt="random picture of {{ $post->author }}</">
             <div class="item-description text-center">
                 <h3>{{ $post->title }}</h3>
                 <h4>{{ $post->author }}</h4>
